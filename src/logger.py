3a9cfafc8e50e926d4020309f9cfd22123a15f00
srikanth449 <srikanth.449@gmail.com>
2025-07-20 00:03:51 +0800

    Exception Logging

---
+++ src/logger.py
@@ -0,0 +1,17 @@
+import logging
+import os
+
+from datetime import datetime
+Log_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",Log_file)
+os.makedirs(logs_path,exist_ok=True)
+
+Log_file_path=os.path.join(logs_path,Log_file)
+
+logging.basicConfig(
+    filename=Log_file_path,
+    format="[%(asctime)s] %(lineno) d %(name)s -%(levelname)s -%(message)s",
+    level=logging.INFO,
+
+)
+
